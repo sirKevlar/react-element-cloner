bdd7cff0ce011baf6b20efd681b71e70f9b489d3
Kev Morel <kev.morel@northcoders.com>
2022-03-15 10:54:02 +0000

    remove logs

--- src/components/DeepRename.jsx
+++ src/components/DeepRename.jsx
@@ -3,7 +3,6 @@ import { cloneElement } from 'react';
 
 export default function DeepRename({ children }) {
   const recursiveCloner = (child) => {
-    console.log(child.props.children, typeof child.props.children);
     return typeof child.props.children === 'string'
       ? cloneElement(child, { children: 'Kev' })
       : recursiveCloner(child.props.children);
